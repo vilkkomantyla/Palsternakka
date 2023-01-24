b9a7cf8ae573e68838ef3448e30d7678d5ad71c8
Your Name <aino.vanhakartano@helsinki.fi>
2023-01-24 13:28:34 +0200

    #4 Started with the query-input

--- searchengine.py
+++ searchengine.py
@@ -1,1 +1,13 @@
 # This is the document for the search engine
+
+
+# Asking user for a query. Feel free to change the function name/location etc.
+
+def getquery():
+    
+    query = input("Enter a search word: ")
+    while len(query) != 0:
+        query = input("Enter a search word: ")
+
+
+
