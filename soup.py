6061821c897de0f9c17b3bfee296d9b9eef46464
vilkkomantyla <ville-veikko.mantyla@helsinki.fi>
2023-01-24 13:16:29 +0200

    Muutokset tähän, (ei toimi)

--- soup.py
+++ soup.py
@@ -3,6 +3,3 @@ from bs4 import BeautifulSoup
 with open("sodexo.htm") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-p = soup.find(string="Tänään")
-s = p.find_all_next(class_="meal_name")
-print(p)
