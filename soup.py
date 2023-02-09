bcb8afe817a8d76293e106577c8b1f82050768ec
Silva Hilosuo <silva.hilosuo@helsinki.fi>
2023-02-09 15:05:59 +0200

    more file arranging

--- soup.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("sodexo.htm") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-
--- textmining.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-URL = "https://www.sodexo.fi/ravintolat/helsingin-yliopisto-paarakennus"
-response = requests.get(URL)
-open("sodexo.htm", "wb").write(response.content)
