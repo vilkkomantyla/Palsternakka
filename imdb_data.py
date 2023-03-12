5713d3fdfe1f9a8cce7849bedc1cb3aefe18dcf5
Silva Hilosuo <silva.hilosuo@helsinki.fi>
2023-03-12 15:22:41 +0200

    kdjfkdjsfsk

--- imdb_data.py
+++ imdb_data.py
@@ -31,7 +31,7 @@ def get_urls():
     rows = soup.tbody.find_all('tr')
     for row in rows:
         urls.append('https://www.imdb.com' + row.a['href'])
-    urls = urls[:8]      # tätä voi muuttaa, jos haluaa testata eri määrällä leffoja tai eri kohdasta rankingia
+    #urls = urls[:8]      # tätä voi muuttaa, jos haluaa testata eri määrällä leffoja tai eri kohdasta rankingia
     return urls
 
 # *********
